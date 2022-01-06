b139e9b8f21eb7b191ab17bea7ae86c0503bb560
Davydenkoen <frontend82@mail.ru>
2022-01-06 18:54:47 +0300

    создал класс Router и подключил его во фронт контроллере

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Requested URL = ' . $_SERVER['QUERY_STRING'];
+//echo 'Requested URL = ' . $_SERVER['QUERY_STRING'];
+
+require __DIR__ . '/../Core/Router.php';
+
+$router = new Router();
+
+echo get_class($router);
